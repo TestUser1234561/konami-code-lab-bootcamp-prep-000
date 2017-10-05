fa093967bc4c93f1cd9df39f74fe8f957ed1bfce
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 06:49:37 +0000

    automatically backed up by learn

---
+++ konami_code.js
@@ -0,0 +1,27 @@
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
+
+function init() {
+
+  const input = document.getElementsByTagName("BODY")[0];
+
+  input.addEventListener('keydown', function(e) {
+    testCode(e.which)
+  })
+
+}
+
+function testCode(key) {
+  key = parseInt(key)
+  if(key == code[index]) {
+    index++;
+
+    if(index == code.length) {
+      alert("Hurray!")
+      index = 0;
+    }
+
+  } else {
+    index = 0;
+  }
+}

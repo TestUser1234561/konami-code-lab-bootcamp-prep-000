c186766e003a34c79e3f0f83e17999be920f30b7
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 06:54:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
-function init() {
+const input = document.getElementsByTagName("BODY")[0];
 
-  const input = document.getElementsByTagName("BODY")[0];
-
-  input.addEventListener('keydown', function(e) {
-    testCode(e.which)
-  })
-
-}
+input.addEventListener('keydown', function(e) {
+  testCode(e.which)
+})
 
 function testCode(key) {
   key = parseInt(key)
